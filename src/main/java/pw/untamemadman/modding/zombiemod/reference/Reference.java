4cd873e1aa815f0dfda57ba577f6a2184f1972c6
untamemadman <samkemp55@gmail.com>
2014-08-27 00:55:11 +0100

    Version 4.3 --Adds Capes

--- src/main/java/pw/untamemadman/modding/zombiemod/reference/Reference.java
+++ src/main/java/pw/untamemadman/modding/zombiemod/reference/Reference.java
@@ -4,9 +4,12 @@ public class Reference
 {
     public static final String MOD_ID = "ZombieMod";
     public static final String MOD_NAME = "TheZombieMod";
-    public static final String VERSION = "1.7.10-4.2";
+    public static final String VERSION = "1.7.10-4.3";
     public static final String ClientProxyClass = "pw.untamemadman.modding.zombiemod.proxy.ClientProxy";
     public static final String ServerProxyClass = "pw.untamemadman.modding.zombiemod.proxy.ServerProxy";
     public static final String GUIFactoryClass = "pw.untamemadman.modding.zombiemod.client.gui.GuiFactory";
-    public static final String DevCapes = "https://raw.githubusercontent.com/untamemadman/MCCapes/master/ZombieMod/capes.json";
+    public static final String DevCapes = "http://pastebin.com/raw.php?i=V2TXrcqq";
 }
+
+// http://pastebin.com/raw.php?i=V2TXrcqq
+// https://raw.githubusercontent.com/untamemadman/MCCapes/master/ZombieMod/capes.json
